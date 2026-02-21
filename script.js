53165bb74e8eb863d75ab3c9596b0b6d0218a5b3
Rizwansh-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-02-21 23:53:39 +0500

    Add index and stylesheet for basic layout

---
+++ script.js
@@ -0,0 +1,10 @@
+const Books=[];
+function Book(title,author,pages,read_status) {
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read_status=read_status;
+}
+function addBooks(){
+    
+}
